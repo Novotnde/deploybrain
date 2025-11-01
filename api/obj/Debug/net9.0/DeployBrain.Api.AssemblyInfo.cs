57301020b104f2867743f91a467297e62074a3c4
denisa <novotnde@hotmail.com>
2025-11-01 19:33:53 +0100

    added to exclude

--- api/obj/Debug/net9.0/DeployBrain.Api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/DeployBrain.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeployBrain.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b8ed4098c8285fbd2b7b566e6503e46845bc86")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeployBrain.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeployBrain.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
